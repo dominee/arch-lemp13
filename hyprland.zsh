31ba9419295fff3f585485a1dd033380ca5dad9b
Dominee Fromhell <dominee@hell.sk>
2024-05-31 14:01:20 +0200

    hyprland placeholder

---
+++ hyprland.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+pacman -Sy hyprland, swaybg swayidle swaylock wlroots wl-clipboard waybar wofi foot mako grim slurp wf-recorder light yad thunar geany mpv mpd mpc viewnior imagemagick xfce-polkit xorg-xwayland xdg-desktop-portal-wlr playerctl pastel python-pywal alacritty rofi pulsemixer
+
+mkdir -p ~/.config/hypr
